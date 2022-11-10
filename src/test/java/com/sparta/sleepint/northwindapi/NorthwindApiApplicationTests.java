dab005c57966db47b32c8fa47469c84d754ddab8
eco999 <justin02_steven@live.co.uk>
2022-11-10 12:16:23 +0000

    initial commit

---
+++ src/test/java/com/sparta/sleepint/northwindapi/NorthwindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sleepint.northwindapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
