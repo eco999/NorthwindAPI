f494b9a15ec762914ff0c40dd765a5da6b6a219f
eco999 <justin02_steven@live.co.uk>
2022-11-11 15:54:43 +0000

    Added /territory/all endpoint

--- src/main/java/com/sparta/sleepint/northwindapi/controller/TerritoryController.java
+++ src/main/java/com/sparta/sleepint/northwindapi/controller/TerritoryController.java
@@ -1,16 +1,21 @@
 package com.sparta.sleepint.northwindapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.sleepint.northwindapi.entity.Employee;
 import com.sparta.sleepint.northwindapi.entity.Territory;
 import com.sparta.sleepint.northwindapi.repositories.EmployeeRepository;
 import com.sparta.sleepint.northwindapi.repositories.TerritoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderDsl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -28,6 +33,7 @@ public class TerritoryController {
     public Set<Employee> getEmployeesByTerritoryId(@PathVariable String territoryId){
         Set<Employee> employeeSet = territoryRepository.findById(territoryId).get().getEmployees();
 
+
         return employeeSet;
     }
 
@@ -40,4 +46,17 @@ public class TerritoryController {
         entityModel.add(webMvcLinkBuilder.withRel("employees"));
         return entityModel;
     }
+
+    @GetMapping("/territory/all")
+    public CollectionModel<List<Territory>> getAllTerritories() {
+        List<Territory> territories = territoryRepository.findAll();
+        CollectionModel <List<Territory>> entityModel = CollectionModel.of(Collections.singleton(territories));
+        //Connect list of all territories to their employees
+        for(Territory territory : territories) {
+            WebMvcLinkBuilder webMvcLinkBuilder = WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).getEmployeesByTerritoryId(territory.getId()));
+            entityModel.add(webMvcLinkBuilder.withRel("employees"));
+        }
+        return entityModel;
+    }
+
 }
